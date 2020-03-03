b6931db82c4c84cb392a3beccd981bd212914493
Mick Groff <mick.groff@gmail.com>
2020-03-03 13:24:10 -0700

    server-and-test

---
+++ api/server.spec.js
@@ -0,0 +1,39 @@
+const server = require('./API/server.js');
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`\n<<< Server running on port ${port} >>>\n`);
+})
+
+const request = require("supertest");
+
+const server = require("./server.js");
+
+it("should set db environment to testing", function () {
+    expect(process.env.DB_ENV).toBe("testing");
+});
+
+describe("server", function () {
+    describe("GET /", function () {
+        it("should return 200", function () {
+            // run the server
+            // make a GET request to /
+            // see that the http code of response is 200
+            return request(server)
+                .get("/")
+                .then(res => {
+                    expect(res.status).toBe(200);
+                });
+        });
+
+        it("should return HTML", function () {
+            return request(server)
+                .get("/")
+                .then(res => {
+                    expect(res.type).toMatch(/html/i);
+                });
+        });
+    });
+});
+
+
---
+++ index.js
@@ -0,0 +1,6 @@
+const server = require('./api/server');
+
+const port = process.env.PORT || 5150;
+server.listen(port,() => {
+    console.log('\n\t\t***********\n\t\t** 5150 **\n\t\t************\n')
+})
